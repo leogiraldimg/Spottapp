c4b3282ae383003cb1df568121694331f3a6fa98
FernandoKGA <fernando.kga@gmail.com>
2019-09-08 12:15:05 -0300

    Fix 'rake spec' spotteds

--- src/app/spec/helpers/spotteds_helper_spec.rb
+++ src/app/spec/helpers/spotteds_helper_spec.rb
@@ -10,6 +10,6 @@ require 'rails_helper'
 #     end
 #   end
 # end
-RSpec.describe SpottedHelper, type: :helper do
+RSpec.describe SpottedsHelper, type: :helper do
   pending "add some examples to (or delete) #{__FILE__}"
 end
