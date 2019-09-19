c3f1baf0a253a936ecf85248e15d2cb7a0e0c0cb
natalypatti <natalypatti@usp.br>
2019-09-18 23:17:45 -0300

    Corrigindo permissoes de acesso do usuario logado

--- src/app/app/controllers/welcome_controller.rb
+++ src/app/app/controllers/welcome_controller.rb
@@ -1,2 +1,8 @@
 class WelcomeController < ApplicationController
+   before_action :require_unlogged_user
+
+   def require_unlogged_user
+    return if !user_signed_in?
+    redirect_to user_path(current_user)
+   end 
 end
