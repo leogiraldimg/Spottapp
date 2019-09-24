7d87191a7d865730b60cc0fcead707bea242c17a
FernandoKGA <fernando.kga@gmail.com>
2019-09-23 23:14:04 -0300

    Criando comments files

--- src/app/db/schema.rb
+++ src/app/db/schema.rb
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 2019_09_21_194746) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
-    t.index ["user_id"], name: "index_collegUseres_on_user_id"
+    t.index ["user_id"], name: "index_colleges_on_user_id"
   end
 
   create_table "spotteds", force: :cascade do |t|
