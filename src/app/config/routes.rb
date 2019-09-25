df29fd4054042e71381d2fd3f2f935c8b3c6660f
FernandoKGA <fernando.kga@gmail.com>
2019-09-24 21:31:46 -0300

    comment base

--- src/app/config/routes.rb
+++ src/app/config/routes.rb
@@ -11,7 +11,9 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create, :show]
   resources :colleges, only: [:new, :create, :show] do
-  resources :spotteds, only: [:new, :create, :show, :index]
-  
+    resources :spotteds, only: [:new, :create, :show, :index]
+  end
+  resources :spotteds, only: [:new, :create, :show, :index] do
+    resources :comments
   end
 end
--- src/app/db/schema.rb
+++ src/app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_21_194746) do
+ActiveRecord::Schema.define(version: 2019_09_24_015930) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,11 @@ ActiveRecord::Schema.define(version: 2019_09_21_194746) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.index ["user_id"], name: "index_colleges_on_user_id"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "spotteds", force: :cascade do |t|
