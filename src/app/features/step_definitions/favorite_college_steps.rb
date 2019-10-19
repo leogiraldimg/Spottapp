f46d9eedafced2a01e848fd840f9f7f2ae0d0218
Lucas <lucaschabariberi@usp.br>
2019-10-19 15:14:27 -0300

    Testes se favoritou uma faculdade

---
+++ src/app/features/step_definitions/favorite_college_steps.rb
@@ -0,0 +1,19 @@
+Given(/^I am on the colleges menu page$/) do
+    visit colleges_path
+  end
+  
+When(/^I click on the star icon$/) do
+    find('.star-white').click
+end
+
+Then(/^The star icon must change to yellow$/) do
+    find('.star')
+end
+
+When(/^I go to favorite colleges menu page$/) do
+    visit favorite_colleges_path
+end
+
+Then(/^I should see the college$/) do
+    find('.block')
+end
