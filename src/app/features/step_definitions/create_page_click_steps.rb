9e71b1e7b1ae0bb0817a8ff3e5dc6c45248b12ec
Lucas <lucaschabariberi@usp.br>
2019-10-19 15:14:13 -0300

    Teste se criou uma faculdade nova

--- src/app/features/step_definitions/create_page_click_steps.rb
+++ src/app/features/step_definitions/create_page_click_steps.rb
@@ -17,4 +17,12 @@ end
 When (/^I click the Cadastrar Nova Página button$/) do
     click_button "Cadastrar"
 end
-  
+
+Given (/^I created a college page$/) do
+    steps %Q{
+        Given I am on the create new page page
+        When I fill the create new page form
+        And I click the Cadastrar Nova Página button
+        Then I should see the initial user page
+    }
+end 
