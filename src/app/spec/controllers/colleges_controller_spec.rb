6c68522953046ddef28c9e98a0a508c4a1024986
leogiraldimg <leogiraldimg@usp.br>
2019-10-20 19:09:40 -0300

    Corrigindo conflito de variáveis

--- src/app/spec/controllers/colleges_controller_spec.rb
+++ src/app/spec/controllers/colleges_controller_spec.rb
@@ -53,7 +53,7 @@ RSpec.describe CollegesController, type: :controller do
     describe "POST #create" do
         before(:each) do
             @current_user = FactoryBot.create(:user)
-            @college = FactoryBot.create(:college)
+            @college_factory = FactoryBot.create(:college)
         end
 
         context "user logged in" do
@@ -62,13 +62,13 @@ RSpec.describe CollegesController, type: :controller do
             end
 
             it "redirected to root_url" do
-                post :create, params: {college: {name: @college.name, initials: @college.initials, city: @college.city, state: @college.state, country: @college.country, unit: @college.unit, user_id: @college.user.id, background_color: @college.background_color, font_family: @college.font_family, background_image: @college.background_image, font_color: @college.font_color, request_to_participate: @college.request_to_participate, admin_approves_spotted: @college.admin_approves_spotted}}
+                post :create, params: {college: {name: @college_factory.name, initials: @college_factory.initials, city: @college_factory.city, state: @college_factory.state, country: @college_factory.country, unit: @college_factory.unit, user_id: @college_factory.user.id, background_color: @college_factory.background_color, font_family: @college_factory.font_family, background_image: @college_factory.background_image, font_color: @college_factory.font_color, request_to_participate: @college_factory.request_to_participate, admin_approves_spotted: @college_factory.admin_approves_spotted}}
 
                 expect(response).to redirect_to(root_url)
             end
 
             it "rendered new" do
-                post :create, params: {college: {name: @college.name, initials: @college.initials, city: @college.city, state: @college.state, country: @college.country, unit: 'ExampleExampleExample', user_id: @college.user.id, background_color: @college.background_color, font_family: @college.font_family, background_image: @college.background_image, font_color: @college.font_color, request_to_participate: @college.request_to_participate, admin_approves_spotted: @college.admin_approves_spotted}}
+                post :create, params: {college: {name: @college_factory.name, initials: @college_factory.initials, city: @college_factory.city, state: @college_factory.state, country: @college_factory.country, unit: 'ExampleExampleExample', user_id: @college_factory.user.id, background_color: @college_factory.background_color, font_family: @college_factory.font_family, background_image: @college_factory.background_image, font_color: @college_factory.font_color, request_to_participate: @college_factory.request_to_participate, admin_approves_spotted: @college_factory.admin_approves_spotted}}
 
                 expect(response).to render_template(:new)
             end
@@ -76,7 +76,7 @@ RSpec.describe CollegesController, type: :controller do
 
         context "user not logged in" do
             it "has a 302 status code" do
-                post :create, params: {college: {name: @college.name, initials: @college.initials, city: @college.city, state: @college.state, country: @college.country, unit: @college.unit, user_id: @college.user.id, background_color: @college.background_color, font_family: @college.font_family, background_image: @college.background_image, font_color: @college.font_color, request_to_participate: @college.request_to_participate, admin_approves_spotted: @college.admin_approves_spotted}}
+                post :create, params: {college: {name: @college_factory.name, initials: @college_factory.initials, city: @college_factory.city, state: @college_factory.state, country: @college_factory.country, unit: @college_factory.unit, user_id: @college_factory.user.id, background_color: @college_factory.background_color, font_family: @college_factory.font_family, background_image: @college_factory.background_image, font_color: @college_factory.font_color, request_to_participate: @college_factory.request_to_participate, admin_approves_spotted: @college_factory.admin_approves_spotted}}
 
                 expect(response.status).to eq(302)
             end
@@ -86,8 +86,8 @@ RSpec.describe CollegesController, type: :controller do
     describe "GET #show" do
         before(:each) do
             @current_user = FactoryBot.create(:user)
-            @college = FactoryBot.create(:college)
-            @college.save
+            @college_factory = FactoryBot.create(:college)
+            @college_factory.save
         end
 
         context "user logged in" do
@@ -96,7 +96,7 @@ RSpec.describe CollegesController, type: :controller do
             end
 
             it "has a 200 status code" do
-                get :show, params: {id: @college.id}
+                get :show, params: {id: @college_factory.id}
 
                 expect(response.status).to eq(200)
             end
@@ -104,7 +104,7 @@ RSpec.describe CollegesController, type: :controller do
 
         context "user not logged in" do
             it "has a 302 status code" do
-                get :show, params: {id: @college.id}
+                get :show, params: {id: @college_factory.id}
 
                 expect(response.status).to eq(302)
             end
@@ -114,8 +114,8 @@ RSpec.describe CollegesController, type: :controller do
     describe "POST #favorite" do
         before(:each) do
             @current_user = FactoryBot.create(:user)
-            @college = FactoryBot.create(:college)
-            @college.save
+            @college_factory = FactoryBot.create(:college)
+            @college_factory.save
         end
         
         context "user logged in" do
@@ -124,21 +124,21 @@ RSpec.describe CollegesController, type: :controller do
             end
 
             it "notice that you have favorited" do
-                post :favorite, params: {id: @college.id, type: "favorite"}
+                post :favorite, params: {id: @college_factory.id, type: "favorite"}
 
                 expect(response).to redirect_to(colleges_path)
                 expect(flash[:notice]).to match(/Você favoritou*/)
             end
 
             it "notice that you have unfavorited" do
-                post :favorite, params: {id: @college.id, type: "unfavorite"}
+                post :favorite, params: {id: @college_factory.id, type: "unfavorite"}
 
                 expect(response).to redirect_to(colleges_path)
                 expect(flash[:notice]).to match(/Você desfavoritou*/)
             end
 
             it "notice that you have done nothing" do
-                post :favorite, params: {id: @college.id, type: ""}
+                post :favorite, params: {id: @college_factory.id, type: ""}
 
                 expect(response).to redirect_to(colleges_path)
                 expect(flash[:notice]).to match(/Nada aconteceu*/)
@@ -147,19 +147,19 @@ RSpec.describe CollegesController, type: :controller do
 
         context "user not logged in" do
             it "notice that you have favorited - has a 302 status code" do
-                post :favorite, params: {id: @college.id, type: "favorite"}
+                post :favorite, params: {id: @college_factory.id, type: "favorite"}
 
                 expect(response.status).to eq(302)
             end
 
             it "notice that you have unfavorited - has a 302 status code" do
-                post :favorite, params: {id: @college.id, type: "unfavorite"}
+                post :favorite, params: {id: @college_factory.id, type: "unfavorite"}
 
                 expect(response.status).to eq(302)
             end
 
             it "notice that you have done nothing - has a 302 status code" do
-                post :favorite, params: {id: @college.id, type: ""}
+                post :favorite, params: {id: @college_factory.id, type: ""}
 
                 expect(response.status).to eq(302)
             end
@@ -169,8 +169,8 @@ RSpec.describe CollegesController, type: :controller do
     describe "POST #update" do
         before(:each) do
             @current_user = FactoryBot.create(:user)
-            @college = FactoryBot.create(:college)
-            @college.save
+            @college_factory = FactoryBot.create(:college)
+            @college_factory.save
         end
 
         context "user logged in" do
@@ -179,14 +179,14 @@ RSpec.describe CollegesController, type: :controller do
             end
 
             it "success updating college settings" do
-                put :update, params: {college: {name: @college.name, initials: @college.initials, city: @college.city, state: @college.state, country: @college.country, unit: @college.unit, user_id: @college.user.id, background_color: @college.background_color, font_family: @college.font_family, background_image: @college.background_image, font_color: @college.font_color, request_to_participate: @college.request_to_participate, admin_approves_spotted: @college.admin_approves_spotted}, id: @college.id}
+                put :update, params: {college: {name: @college_factory.name, initials: @college_factory.initials, city: @college_factory.city, state: @college_factory.state, country: @college_factory.country, unit: @college_factory.unit, user_id: @college_factory.user.id, background_color: @college_factory.background_color, font_family: @college_factory.font_family, background_image: @college_factory.background_image, font_color: @college_factory.font_color, request_to_participate: @college_factory.request_to_participate, admin_approves_spotted: @college_factory.admin_approves_spotted}, id: @college_factory.id}
 
-                expect(response).to redirect_to(edit_college_path(@college))
+                expect(response).to redirect_to(edit_college_path(@college_factory))
                 expect(flash[:success]).to match(/Dados atualizados*/)
             end
             
             it "failed updating college settings - render edit college" do
-                put :update, params: {college: {name: @college.name, initials: @college.initials, city: @college.city, state: @college.state, country: @college.country, unit: @college.unit, user_id: @college.user.id, background_color: "12", font_family: @college.font_family, background_image: @college.background_image, font_color: @college.font_color, request_to_participate: @college.request_to_participate, admin_approves_spotted: @college.admin_approves_spotted}, id: @college.id}
+                put :update, params: {college: {name: @college_factory.name, initials: @college_factory.initials, city: @college_factory.city, state: @college_factory.state, country: @college_factory.country, unit: @college_factory.unit, user_id: @college_factory.user.id, background_color: "12", font_family: @college_factory.font_family, background_image: @college_factory.background_image, font_color: @college_factory.font_color, request_to_participate: @college_factory.request_to_participate, admin_approves_spotted: @college_factory.admin_approves_spotted}, id: @college_factory.id}
 
                 expect(response).to render_template(:edit)
             end
@@ -194,7 +194,7 @@ RSpec.describe CollegesController, type: :controller do
         
         context "user not logged in" do
             it "has a 302 status code" do
-                put :update, params: {college: {name: @college.name, initials: @college.initials, city: @college.city, state: @college.state, country: @college.country, unit: @college.unit, user_id: @college.user.id, background_color: @college.background_color, font_family: @college.font_family, background_image: @college.background_image, font_color: @college.font_color, request_to_participate: @college.request_to_participate, admin_approves_spotted: @college.admin_approves_spotted}, id: @college.id}
+                put :update, params: {college: {name: @college_factory.name, initials: @college_factory.initials, city: @college_factory.city, state: @college_factory.state, country: @college_factory.country, unit: @college_factory.unit, user_id: @college_factory.user.id, background_color: @college_factory.background_color, font_family: @college_factory.font_family, background_image: @college_factory.background_image, font_color: @college_factory.font_color, request_to_participate: @college_factory.request_to_participate, admin_approves_spotted: @college_factory.admin_approves_spotted}, id: @college_factory.id}
 
                 expect(response.status).to eq(302)                
             end
