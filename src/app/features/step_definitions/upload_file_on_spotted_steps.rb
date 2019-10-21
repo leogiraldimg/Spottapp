b796845e63fab9bc724d062dbb41fcf0b063a52c
Lucas <lucaschabariberi@usp.br>
2019-10-20 21:36:49 -0300

    Testes de upload de imagem

---
+++ src/app/features/step_definitions/upload_file_on_spotted_steps.rb
@@ -0,0 +1,12 @@
+When("I upload an image to the spotted") do
+    page.attach_file "spotted[images][]", Rails.root.join('features', 'images', 'cat_and_dog.jpg')
+end
+
+When("I go to spotted page") do
+    @spotted = Spotted.find_by(content: "Spotted Teste")
+    visit("/colleges/college_id/spotteds/#{@spotted.id}")
+end
+
+Then("I should my see my image attached") do
+    find('#image')
+end
