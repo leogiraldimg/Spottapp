8f4d34ca15b9230e9cb9b4ebbebe9eb251eddcd4
Lucas <lucaschabariberi@usp.br>
2019-10-21 22:01:51 -0300

    Adição de imagem responsiva

--- src/app/app/models/spotted.rb
+++ src/app/app/models/spotted.rb
@@ -1,6 +1,12 @@
 class Spotted < ApplicationRecord
 
-    has_attached_file :image, :styles => { :original => "350x350>" }
+    has_attached_file :image,
+        :styles => {
+            :thumb => "60x60>",
+            :small => "140x140>",
+            :medium =>   "310x310>",
+            :large => "500x500>"
+        }
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
     validates :content, presence: true, length: {minimum: 1}    
