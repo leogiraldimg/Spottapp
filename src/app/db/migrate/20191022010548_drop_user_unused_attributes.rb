1e69c39546042e2bd0081f7b83eb1734bd1cf439
leogiraldimg <leogiraldimg@usp.br>
2019-10-22 22:59:58 -0300

    Removendo arquivo de migração

--- src/app/db/migrate/20191022010548_drop_user_unused_attributes.rb
+++
@@ -1,8 +0,0 @@
-class DropUserUnusedAttributes < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :users, :token
-    remove_column :users, :uid
-    remove_column :users, :name
-    remove_column :users, :avatar
-  end
-end
