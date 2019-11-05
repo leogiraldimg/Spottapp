043b769b1fc4f108e49934f064fefbfe4b7456a8
FernandoKGA <fernando.kga@gmail.com>
2019-11-05 20:53:42 -0300

    Criando model

---
+++ src/app/spec/factories/college_whitelists.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :college_whitelist do
+    
+  end
+end
---
+++ src/app/spec/models/college_whitelist_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe CollegeWhitelist, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
