ebf6720f99e937ada644fe074960d37e5b7a26f9
higormoles <higor_moles@usp.br>
2019-11-05 00:09:52 -0300

    teste para model e controller Administrator

--- src/app/app/controllers/administrators_controller.rb
+++ src/app/app/controllers/administrators_controller.rb
@@ -1,5 +1,6 @@
 class AdministratorsController < ApplicationController
-    
+    before_action :require_logged_in_user
+
     def setting_manager
         @college = College.find(params[:college])
         if !(user_found?)
@@ -7,6 +8,8 @@ class AdministratorsController < ApplicationController
         else
             if (user_is_already_administrator?)
                 flash[:notice] = "Usuário já é administrador"
+            elsif (user_is_owner_of_the_page?)
+                flash[:notice] = "Usuário é o criador e administrador da página"
             else
                 @user = User.find_by(nickname: params[:user][:nickname])
                 @college.administrator.create(user_id: @user.id)
@@ -26,6 +29,11 @@ class AdministratorsController < ApplicationController
     def user_is_already_administrator?
         @user = User.find_by(nickname: params[:user][:nickname])
         Administrator.where(user_id: @user.id, college_id: @college.id).exists?
+    end
+
+    def user_is_owner_of_the_page?
+        @user = User.find_by(nickname: params[:user][:nickname])
+        College.where(user_id: @user.id, id: @college.id).exists?
     end
 
 end
