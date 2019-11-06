518256739887187ce904f3c3c53249980427f243
leogiraldimg <leogiraldimg@usp.br>
2019-11-05 23:46:45 -0300

    Implementando "Escolher minhas páginas favoritas baseado em minha localização" + testes unitários + testes funcionais

--- src/app/spec/controllers/colleges_controller_spec.rb
+++ src/app/spec/controllers/colleges_controller_spec.rb
@@ -170,7 +170,6 @@ RSpec.describe CollegesController, type: :controller do
         before(:each) do
             @current_user = FactoryBot.create(:user)
             @college = FactoryBot.create(:college)
-            @college.save
         end
 
         context "user logged in" do
@@ -215,6 +214,33 @@ RSpec.describe CollegesController, type: :controller do
                     @college.reload
                 }.to raise_error ActiveRecord::RecordNotFound
              end
+        end
+    end
+
+    describe "GET #per_area" do
+        before(:each) do
+            @current_user = FactoryBot.create(:user)
+            @college = FactoryBot.create(:college)
+        end
+
+        context "user logged in" do
+            before(:each) do
+                session[:user_id] = @current_user.id
+            end
+            
+            it "has a 200 status code" do
+                get :per_area, params: {state: @current_user.state}
+
+                expect(response.status).to eq(200)                
+            end
+        end
+
+        context "user not logged in" do
+            it "has a 302 status code" do
+                get :per_area
+
+                expect(response.status).to eq(302)
+            end
         end
     end
 end
