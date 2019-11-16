90be836f3be429642ff4aa89c3cf5cfb4b4502a3
Lucas <lucaschabariberi@usp.br>
2019-11-16 19:45:43 -0300

    New user menu layout

--- src/app/app/assets/javascripts/application.js
+++ src/app/app/assets/javascripts/application.js
@@ -15,3 +15,30 @@
 //= require bootstrap-sprockets
 //= require_tree .
 //= require serviceworker-companion
+
+function changeMenuLayout() {
+    if(document.getElementById("btn-change-user-menu").value == "circle"){
+        document.getElementById("btn-change-user-menu").value = "list";
+        document.getElementById("circle-menu").style.display = "none";
+        document.getElementById("list-menu").style.display = "inline-block";
+    }
+     
+    else if(document.getElementById("btn-change-user-menu").value == "list"){
+        document.getElementById("btn-change-user-menu").value = "circle";
+        document.getElementById("circle-menu").style.display = "inline-block";
+        document.getElementById("list-menu").style.display = "none";
+    }
+}
+
+function resizeIcons() {
+    if($(window).width() > 400) {
+        $('.user#show #icon').addClass('fa-2x');
+        $('.user#show #icon').removeClass('fa-lg');
+    }else{
+        $('.user#show #icon').addClass('fa-lg');
+        $('.user#show #icon').removeClass('fa-2x');
+    }
+}
+
+$( window ).on( "load", resizeIcons );
+$( window ).on( "resize", resizeIcons );
