eed9d72f1005fb8dcb2e7e050e4975dea7798276
higormoles <higor_moles@usp.br>
2019-11-30 02:11:43 -0300

    Refatoração aplication.js para correção de mal cheiro de código

--- src/app/app/assets/javascripts/application.js
+++ src/app/app/assets/javascripts/application.js
@@ -35,36 +35,27 @@ function resizeIcons() {
 $( window ).on( 'load', resizeIcons );
 $( window ).on( 'resize', resizeIcons );
 
+var htmlClasses = ['.favorite_colleges#show .jumbotron .collegebox .college', '.colleges#index .jumbotron .collegebox .college'];
+
 $(document).ready(function(){
-    $('.favorite_colleges#show .jumbotron .collegebox .college').click(function() {
-        var $this = $(this).children('.details');
 
-        var openDetails = $('.favorite_colleges#show .jumbotron .collegebox .college .details').not($this).filter(function () { 
-            return this.style.display == 'block' 
+    for(var i = 0; i<this.htmlClasses.lenght; i++) {
+
+        $(htmlClasses[i]).click(function() {
+            var $this = $(this).children('.details');
+    
+            var openDetails = $( htmlClasses[i] + ' .details').not($this).filter(function () { 
+                return this.style.display == 'block' 
+            });
+            openDetails.css('display', 'none');
+    
+            if( $this.css('display') === 'block' ){
+                $this.css('display', 'none');
+            } else {
+                $this.css('display', 'block');
+            }
         });
-        openDetails.css('display', 'none');
-
-        if( $this.css('display') === 'block' ){
-            $this.css('display', 'none');
-        } else {
-            $this.css('display', 'block');
-        }
-    });
-
-    $('.colleges#index .jumbotron .collegebox .college').click(function() {
-        var $this = $(this).children('.details');
-
-        var openDetails = $('.colleges#index .jumbotron .collegebox .college .details').not($this).filter(function () { 
-            return this.style.display == 'block' 
-        });
-        openDetails.css('display', 'none');
-
-        if( $this.css('display') === 'block' ){
-            $this.css('display', 'none');
-        } else {
-            $this.css('display', 'block');
-        }
-    });
+    }
 });
 
 $(document).ready(function(){
