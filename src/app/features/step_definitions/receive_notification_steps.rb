dd256da4a92d003f8294a36eb5dd3ac32bf15274
natalypatti <natalypatti@usp.br>
2019-12-04 03:36:28 -0300

    receive_notification caminho triste

--- src/app/features/step_definitions/receive_notification_steps.rb
+++ src/app/features/step_definitions/receive_notification_steps.rb
@@ -5,3 +5,7 @@ end
 Then("I should see my notifications") do
   expect(page).to have_xpath('.//div', text: "Novo comentário")
 end
+
+Then("I shouldnt see my notifications") do
+  expect(page).not_to have_xpath('.//div', text: "Novo comentário")
+end
